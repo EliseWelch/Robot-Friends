da629f1cb8f18177d17a7836ee2b9615d54001a3
Elise Welch <eliseanyawelch@gmail.com>
2021-02-16 15:18:46 -0500

    Adding React Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../containers/CardList';
 import SearchBox from '../containers/SearchBox';
 import Scroll from '../containers/Scroll';
 import './App.css';
 
 
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-		  robots: [],
-	 	  searchfield: ''
-		}
-	}
+const App = () => {
 
-	componentDidMount() {
-	   fetch('https://jsonplaceholder.typicode.com/users')
+	const [robots, setRobots] = useState([]);
+	const [searchField, setSearchField] = useState('');
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
 	     .then(response=> response.json())
-	     .then(users => {this.setState({ robots: users})});
-    }
+	     .then(users => setRobots(users));
+	})
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
+	const onSearchChange = (event) => {
+		setSearchField(event.target.value);
 	}
 
-
-	render() {
-	  const { robots, searchfield } = this.state;
-	  const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-	  })	
-	  return !robots.length ?
-      <h1 className='tc f2'>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-	}
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
+	})	
+	return !robots.length ?
+	<h1 className='tc f2'>Loading</h1> :
+	(
+	<div className='tc'>
+		<h1 className='f1'>RoboFriends</h1>
+		<SearchBox searchChange={onSearchChange}/>
+		<Scroll>
+		<CardList robots={filteredRobots} />
+		</Scroll>
+	</div>
+	);
 }
 
 
